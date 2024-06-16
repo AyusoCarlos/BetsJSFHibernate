98a19613aac97250e33c9c91e69309fbf7fd744e
AyusoCarlos <132440940+AyusoCarlos@users.noreply.github.com>
2024-06-16 12:34:38 +0200

    Update QueryBean.java

--- BetsJSFHibernate/src/main/java/modelo/bean/QueryBean.java
+++ BetsJSFHibernate/src/main/java/modelo/bean/QueryBean.java
@@ -1,9 +1,20 @@
 package modelo.bean;
 
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Vector;
+
+import javax.annotation.ManagedBean;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+
+import org.primefaces.event.SelectEvent;
+
 import java.util.Date;
 
 import businessLogic.BLFacade;
@@ -11,7 +22,8 @@ import businessLogic.BLFacadeImplementation;
 import domain.Event;
 import domain.Question;
 
-
+@ManagedBean(value = "queryBean")
+@ViewScoped
 public class QueryBean {
     
 
@@ -21,9 +33,16 @@ public class QueryBean {
     
    
     private QuestionBean question;
+    
+    private Date eventDate;
 
     
     private BLFacade businessLogic;
+    
+    
+    private List<Event> events;
+    
+    private String eventNumber;
 
    
     private String event;
@@ -37,7 +56,15 @@ public class QueryBean {
         this.questions = new ArrayList<Question>();
     }
 
-    // Getters y Setters
+    public String getEventNumber() {
+		return eventNumber;
+	}
+
+	public void setEventNumber(String eventNumber) {
+		this.eventNumber = eventNumber;
+	}
+
+	// Getters y Setters
     public Long getId() {
         return id;
     }
@@ -69,6 +96,10 @@ public class QueryBean {
     public void setEvent(String event) {
         this.event = event;
     }
+    
+    public List<Event> getEvents() {
+        return events;
+    }
 
     public List<Question> getQuestions() {
         return questions;
@@ -79,8 +110,8 @@ public class QueryBean {
     }
 
     public void queryQuestions() {
-        Date currentDate = new Date(0);
-        List<Event> events = businessLogic.getEvents(currentDate); // Obtener todos los eventos
+    	LlenarEvent();
+        List<Event> events = businessLogic.getEvents(this.eventDate); // Obtener todos los eventos
         questions = new ArrayList<Question>();
         
         for (Event e : events) {
@@ -88,17 +119,47 @@ public class QueryBean {
                 questions.addAll(e.getQuestions());
             }
         }
+        
     }
-    public List<Event> availableEvents() {
-        Date currentDate = new Date(); // Obtener la fecha actual
-        List<Date> datesWithEvents = businessLogic.getEventsMonth(currentDate); // Obtener los días del mes con eventos
+    
+    
+    public void LlenarEvent() {
+    	Event e = null;
+    	try {
+    		for (Event i : events) {
+    			if((Integer.valueOf(eventNumber).intValue()==i.getEventNumber().intValue())) {
+    				e = i;
+    			}
+    			
+    		}
+    	if (e == null) {
+    		System.out.println("NO existe");
+    	}else {
+    		System.out.println("SI existe"+ e);
+    		event = e.getDescription();
+    	}
+    		
+    		
+    	}catch(Exception e1){
+    		
+    	}
+    }
+    // Método para obtener la lista de eventos disponibles en la fecha actual
+    public void updateEvents() {
+            this.events = businessLogic.getEvents(this.eventDate);
+            System.out.println(this.eventDate);
+            System.out.println(this.events);
+            setEventNumber(null);
+       
+    }
+    
+    public void onDateSelect(SelectEvent event) {
+    	FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Fecha escogida: " + event.getObject()));
+        System.out.println(event.getObject());
+        eventDate =Date.from(((LocalDate) event.getObject()).atStartOfDay(ZoneId.systemDefault()).toInstant());
         
-        List<Event> allEvents = new ArrayList<Event>();
-        for (Date date : datesWithEvents) {
-            List<Event> events = businessLogic.getEvents(date); // Obtener los eventos para cada día
-            allEvents.addAll(events);
-        }
-        
-        return allEvents;
+        updateEvents();
+        setEventNumber(null);
+       
     }
 }
